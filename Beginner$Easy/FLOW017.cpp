844667efaba4ffa81585986e1c1a71155d3abb6a
keatshackon <keatshackon@gmail.com>
2020-10-06 18:03:36 +0530

    FLOW017.cpp added

--- Beginner$Easy/FLOW017.cpp
+++ Beginner$Easy/FLOW017.cpp
@@ -4,17 +4,22 @@ typedef long long ll;
 
 int main()
 {
-	ll a,b,c;
-	cin>>a>>b>>c;
+	ll a,b,c,t;
+	cin>>t;
+	while(t--){
+		cin>>a>>b>>c;
 
-	if(b>=a && b<=c || b<=a && b>=c){
-		cout<<b<<"\n";
+		if(b>=a && b<=c || b<=a && b>=c){
+			cout<<b<<"\n";
+		}
+		else if(a>=b && a<=c || a<=b && a>=c){
+			cout<<a<<"\n";
+		}
+		else if(c>=a && c<=b || c<=a && c>=b){
+			cout<<c<<"\n";
+		}	
 	}
-	else if(a>=b && a<=c || a<=b && a>=c){
-		cout<<a<<"\n";
-	}
-	else if(c>=a && c<=b || c<=a && c>=b){
-		cout<<c<<"\n";
-	}
+	
+	return 0;
 
 }
