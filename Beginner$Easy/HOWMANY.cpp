c7d3f6b9d31e2dce56cc38fe17d89a31ee4ee699
keatshackon <keatshackon@gmail.com>
2020-10-08 08:47:17 +0530

    HOW MANY.CPP added

--- Beginner$Easy/HOWMANY.cpp
+++ Beginner$Easy/HOWMANY.cpp
@@ -6,8 +6,14 @@ int main()
 {
 	ll t,n,i,h,j,k;
 	cin>>t;
-	while(t--){
-		cin>>n;
-		
+	if(t<10){
+		cout<<"1"<<"\n";
+	}else if(t<100 && t>=10){
+		cout<<"2"<<"\n";
+	}else if(t<1000){
+		cout<<"3"<<"\n";
+	}else{
+		cout<<"More than 3 digits"<<"\n";
 	}
+	return 0;
 }
