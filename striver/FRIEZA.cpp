1f42423e5b95c202e188230a35d342e1a93c6740
keatshackon <keatshackon@gmail.com>
2021-01-21 09:52:31 +0530

    FRIEZA.cpp added

--- striver/FRIEZA.cpp
+++ striver/FRIEZA.cpp
@@ -9,21 +9,36 @@ int main()
 	cin>>t;
 	while(t--){
 		cin>>s;
-		string res="";
-		for(int i=0;i<s.size()-1;i++){
-			int cnt = 0;
-			int j=i;
-			while(j<s.size()){
-				size_t found = s.find(s[i]); 
-				 if (!(found != string::npos)) {
-				 	cnt++;
-				 }else{
-				 	break;
-				 }
-				 j++;
+		string cmp = "frieza";
+		string res = "";
+		int cnt = 0,cnt1 = 0;
+		for(int i = 0;i < s.length();i++){
+
+			int k =  cmp.find(s[i]);
+
+			if(k != -1){
+				if(cnt1 != 0){
+					res+=to_string(cnt1);
+					cnt1 = 0;
+				}
+				cnt++;
+				
+			
+			}else{
+				if(cnt != 0){
+					res+=to_string(cnt);
+					cnt = 0;
+				}
+				cnt1++;
+				
 			}
-			res+= to_string(cnt);
+		}
+		if(cnt != 0){
+			res+=to_string(cnt);
+		}else{
+			res+=to_string(cnt1);
 		}
 		cout<<res<<"\n";
 	}
+	
 }
---
+++ striver/e.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s = "keats";
+    int i = s.find('z');
+    cout<<i;
+
+}
